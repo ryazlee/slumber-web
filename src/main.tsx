321fa5abb7b31408dc5b96c66d1d152b81a57326
ryan <ryan.j.lee99@gmail.com>
2026-06-08 20:52:04 -0700

    Revert "Fix?"
    This reverts commit 99b3300e8437f3478d784d2fdc3a5e4163a52a60.

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/global.css';
 
-const basename = import.meta.env.BASE_URL.replace(/\/$/, '') || '/';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
